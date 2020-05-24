6eb10f4445d071a44f24b4b3f4f0be9e65ead2ad
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-24 20:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-
+function holidayCountdown(holiday, days) {
+  
+}
+         return `It's ${days} days until ${holiday}!`
 
 
          
